8fa31542a0d4e9eaab08a215dccb90b792f67d76
SUBU VLSI <121168850+SUBUVLSI@users.noreply.github.com>
2023-02-16 13:17:25 +0300

    Update laplacian_tb.v

--- laplacian_tb.v
+++ laplacian_tb.v
@@ -5,12 +5,7 @@ module laplacian_test(
   output [7:0] okunan_veri
 
 );
-//ram bağlantısı
-  // reg [7:0] px;
-  // reg [7:0] data_ram;
-  // reg [17:0] addr_ram=0;
-  // reg we_ram=0;
-// gauss ve sobel alt bağlantıları
+
   reg en_l; 
   reg [7:0] data_i0_l  ;
   reg [7:0] data_i1_l  ;
@@ -26,9 +21,9 @@ module laplacian_test(
   parameter deger=77924;
   parameter deger1=76800;
   parameter deger2=75684;
-  reg [7:0] mem[0:deger1-1]; //ilk piksel değeri için 76800
-  reg [7:0] ram[0:deger1-1]; //ilk pikseş değeri için 76800
-  reg [7:0] ram1[0:deger-1]; //gauss yaptık 76800
+  reg [7:0] mem[0:deger1-1]; //ilk piksel deÃ°eri iÃ§in 76800
+  reg [7:0] ram[0:deger1-1]; //ilk pikseÃ¾ deÃ°eri iÃ§in 76800
+  reg [7:0] ram1[0:deger-1]; 
   reg [7:0] ram2[0:deger1-1];
   reg [7:0] core_ram_out[0:8];
   reg [7:0] durum=0;
@@ -62,7 +57,7 @@ module laplacian_test(
   end
   initial begin 
     #77000
-    f=$fopen("D:/laplacian_test.txt","w"); //  ?C:/Users/korze/Downloads/asil_resim_degerleri.txt
+    f=$fopen("D:/laplacian_test.txt","w"); 
   end
   initial begin
         #6100000
@@ -96,7 +91,7 @@ always @(posedge clk_i) begin
                  
             end
             1:begin 
-                if(ind<deger1)begin // piksel değerlerini diziye aktarıyor
+                if(ind<deger1)begin // piksel deÃ°erlerini diziye aktarÃ½yor
                     ram[ind]<=mem[ind];
                     ind<=ind+1;
                 end else begin
@@ -108,7 +103,7 @@ always @(posedge clk_i) begin
 
 
             2:begin
-                     if(sayac<2)begin // 3x3 lük değerleri alıyor
+                     if(sayac<2)begin // 3x3 lÃ¼k deÃ°erleri alÃ½yor
                         core_ram_out[0] <= ram[ind_l]; 
                         core_ram_out[1] <= ram[ind_l+1];
                         core_ram_out[2] <= ram[ind_l+2];
@@ -126,9 +121,9 @@ always @(posedge clk_i) begin
                         durum=3;
                       end
             end 
-            3:begin // gauss yapıyor
+            3:begin // laplacian yapÃ½yor
                 if(en_l==1)begin 
-                    if(sayac<36)begin // gauss işlemine sokuyor
+                    if(sayac<36)begin 
                   
                         data_i0_l <= core_ram_out[0]  ;
                         data_i1_l <= core_ram_out[1]  ;
@@ -146,14 +141,14 @@ always @(posedge clk_i) begin
                         durum=4;  
                     end
                 end else begin
-                durum=7; // durumu en sona bağla
+                durum=7; 
                 end
             end 
             4:begin
                if(ram1_ind<75684)begin 
                    if(ind<318)begin
                        if(sonuc_done_l==1)begin
-                           ram1[ram1_ind]<=data_o_l; //değerleri diziye yazıyor
+                           ram1[ram1_ind]<=data_o_l; //deÃ°erleri diziye yazÃ½yor
                            ram1_ind<=ram1_ind+1;
                            ind_l<=ind_l+1;
                            ind<=ind+1;
@@ -173,7 +168,6 @@ always @(posedge clk_i) begin
                    ram1_ind<=0;
                    ind_l<=0;
                    sayac<=0;
-                   //sonuc_done_g<=0;
                    ind<=0; 
                    durum<=5;
                end 
@@ -205,7 +199,6 @@ always @(posedge clk_i) begin
                 if(ind<2)begin
                    ram2[sayac+321]<=0;
                    ind<=ind+1;
-                   //ram2_ind<=ram2_ind+1;
                    sayac<=sayac+1;
                end else begin
                    ind<=0;
@@ -214,6 +207,13 @@ always @(posedge clk_i) begin
                end
             end
              7:begin
+                sayac<=0;
+                ind<=0;
+                ic_sayac<=0;
+                ram_ind<=0;
+                ram1_ind<=0;
+                ram2_ind<=0;
+                ind_m<=0; 
              end
              
 
@@ -221,12 +221,5 @@ always @(posedge clk_i) begin
       end 
    end
 endmodule           
-          
-//   ram ram_sonuc(
-//.aclk(clk_i),
-//.data_in(px),
-//.addr(addr_ram),
-//.we(we_ram),
-//.data_out(data_ram)
-//);      
+    
                 

04e5b4862cf2cdc4b3a6ade63124975dbf3ca452
SUBU VLSI <121168850+SUBUVLSI@users.noreply.github.com>
2023-04-14 12:03:21 +0300

    Update gorev6_top_tb.v

--- gorev6_top_tb.v
+++ gorev6_top_tb.v
@@ -1,23 +1,5 @@
 `timescale 100ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2023 14:45:34
-// Design Name: 
-// Module Name: gorev6_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module gorev6_top(

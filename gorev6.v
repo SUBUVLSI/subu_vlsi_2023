3786bb391553beaa346d4e458522d785637f7be6
SUBU VLSI <121168850+SUBUVLSI@users.noreply.github.com>
2023-04-14 12:02:52 +0300

    Update gorev6.v

--- gorev6.v
+++ gorev6.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2023 14:58:30
-// Design Name: 
-// Module Name: gorev6
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module gorev6(
